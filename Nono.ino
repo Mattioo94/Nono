7d30c6ba4b2949302597bdb381b84700279dae65
Mattioo <mattioo@wp.pl>
2017-10-13 19:21:24 +0200

    Update Nono.ino
    Add servos to control

--- Nono.ino
+++ Nono.ino
@@ -3,6 +3,7 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
+#include <Servo.h> 
 
 //================================================================================================
 // NETWORK
@@ -14,6 +15,13 @@ const char* password = "12345678";
 ESP8266WebServer server(80);
 
 //================================================================================================
+// SERVOS
+//================================================================================================
+
+Servo servoH; // HORIZONTAL
+Servo servoV; // VERTICAL
+
+//================================================================================================
 // GPIO's
 //================================================================================================
 
@@ -23,6 +31,9 @@ int LEFT_GPIO_PIN = 5;
 int RIGHT_PWM_PIN = 13;
 int RIGHT_GPIO_PIN = 15;
 
+int SERVO_HORIZONTAL_PIN = 14;
+int SERVO_VERTICAL_PIN = 16;
+
 //================================================================================================
 // CURRENT VALUES
 //================================================================================================
@@ -30,6 +41,9 @@ int RIGHT_GPIO_PIN = 15;
 int LEFT_VALUE = 0;
 int RIGHT_VALUE = 0;
 int COURSE_VALUE = 1;
+
+int SERVO_HORIZONTAL_VALUE = 90;
+int SERVO_VERTICAL_VALUE = 90;
 
 //================================================================================================
 // SETUP
@@ -49,7 +63,13 @@ void setup(void)
   digitalWrite(RIGHT_PWM_PIN, LOW);
   digitalWrite(RIGHT_GPIO_PIN, LOW);
 
+  servoH.attach(SERVO_HORIZONTAL_PIN);
+  servoV.attach(SERVO_VERTICAL_PIN);
+
   //----------------------------------------------------------------------------------------------
+
+  servoH.write(SERVO_HORIZONTAL_VALUE);
+  servoV.write(SERVO_VERTICAL_VALUE);
   
   server.on("/", HTTP_POST, []()
   {
@@ -93,17 +113,23 @@ void Configure(JsonObject& json)
   if(json.containsKey("Nono") && json["Nono"].is<JsonArray&>())
   { 
     JsonArray& CONFIG = json["Nono"].asArray();
-    if(CONFIG.size() == 3 && CONFIG[0].is<int>() && CONFIG[1].is<int>() && CONFIG[2].is<int>())
+    if(CONFIG.size() == 5 && CONFIG[0].is<int>() && CONFIG[1].is<int>() && CONFIG[2].is<int>() && CONFIG[3].is<int>() && CONFIG[4].is<int>())
     {
       int LEFT = CONFIG[0];
       int RIGHT = CONFIG[1];
       int COURSE = CONFIG[2];
+
+      int SERVO_HORIZONTAL = CONFIG[3];
+      int SERVO_VERTICAL = CONFIG[4];
       
-      if(LEFT >= 0 && LEFT <= 1024 && RIGHT >= 0 && RIGHT <= 1024 && COURSE >= 0 && COURSE <= 1)
+      if(LEFT >= 0 && LEFT <= 1024 && RIGHT >= 0 && RIGHT <= 1024 && COURSE >= 0 && COURSE <= 1 && SERVO_HORIZONTAL >= 0 && SERVO_HORIZONTAL <= 180 && SERVO_VERTICAL >= 0 && SERVO_VERTICAL <= 180)
       {
         LEFT_VALUE = LEFT;
         RIGHT_VALUE = RIGHT;
         COURSE_VALUE = COURSE;
+
+        SERVO_HORIZONTAL_VALUE = SERVO_HORIZONTAL;
+        SERVO_VERTICAL_VALUE = SERVO_VERTICAL;
 
         digitalWrite(RIGHT_GPIO_PIN, COURSE_VALUE == 0 ? LOW : HIGH);
         digitalWrite(LEFT_GPIO_PIN, COURSE_VALUE == 0 ? LOW : HIGH);
@@ -111,7 +137,10 @@ void Configure(JsonObject& json)
         analogWrite(RIGHT_PWM_PIN, COURSE_VALUE == 0 ? LEFT_VALUE : 1024 - LEFT_VALUE);
         analogWrite(LEFT_PWM_PIN, COURSE_VALUE == 0 ? RIGHT_VALUE : 1024 - RIGHT_VALUE);
 
-        String json = "{\"Nono\":[" + String(LEFT_VALUE) + ", " + String(RIGHT_VALUE) + ", " + String(COURSE_VALUE) + "]}";  
+        servoH.write(SERVO_HORIZONTAL_VALUE);
+        servoV.write(SERVO_VERTICAL_VALUE);
+
+        String json = "{\"Nono\":[" + String(LEFT_VALUE) + ", " + String(RIGHT_VALUE) + ", " + String(COURSE_VALUE) + ", " + String(SERVO_HORIZONTAL_VALUE) + ", " + String(SERVO_VERTICAL_VALUE) + "]}";  
         server.send( 200, "text/json", json );
       }
     }
